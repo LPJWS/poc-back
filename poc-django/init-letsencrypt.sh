98418a231744fb2bc3923252035da21874ad07d6
Daniil Volkov <lpjakewolfskin@gmail.com>
2021-10-29 11:41:47 +0300

    add ssl

--- poc-django/init-letsencrypt.sh
+++ poc-django/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(happy10k.ru www.happy10k.ru)
+domains=(pieceofchit.xyz www.pieceofchit.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
